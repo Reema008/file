4adbb12bc679385128fe312f9fa49563bed2ea7b
Reema008 <48449970+Reema008@users.noreply.github.com>
2019-08-05 18:42:02 +0530

    Add files via upload

---
+++ fileupload.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import * as XLSX from 'xlsx';
+@Component({
+  selector: 'app-fileupload',
+  templateUrl: './fileupload.component.html',
+  styleUrls: ['./fileupload.component.css']
+})
+export class FileuploadComponent implements OnInit {
+
+  constructor() { }
+  onFileChange(evt: any) {
+    const target: DataTransfer =  (evt.target) as DataTransfer;
+    if (target.files.length !== 1) { throw new Error('Cannot use multiple files'); }
+    const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+      const bstr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(bstr, {type: 'binary'});
+      const wsname: string = wb.SheetNames[0];
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+      const data = XLSX.utils.sheet_to_json(ws);
+      console.log(data);
+    };
+    reader.readAsBinaryString(target.files[0]);
+  }
+
+  ngOnInit() {
+  }
+
+}
